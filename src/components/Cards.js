40988aac96b6703e8e14b9ded1f9901dc19c4e0b
EdithRincon <andre-lin@hotmail.es>
2021-11-20 00:03:23 -0500

    Carrito

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,13 +1,28 @@
 import React, { useState } from 'react'
 import { Card, Button, Container } from 'react-bootstrap';
-import { ModalProduct } from './Modal';
-
 
 
 
 export const Cards = ({productos}) => {
-    const [modalShow, setModalShow] = useState(false);
-
+    
+    const onSearchid = (e) => {
+        console.log(e.target.id);
+        const itemName=e.target.id;
+        const Todos = productos.find(todo => todo.description===itemName);
+        const localStorageItem = localStorage.getItem("items");
+        let parsedItem=[];
+        if (!!localStorageItem) {
+           parsedItem = JSON.parse(localStorage.getItem("items"))
+           parsedItem.push(Todos)
+          localStorage.setItem("items", JSON.stringify(parsedItem));
+          JSON.parse(localStorageItem)
+          parsedItem =Todos;
+        } else {
+            parsedItem.push(Todos)
+            localStorage.setItem("items", JSON.stringify(parsedItem));
+        }
+    
+    };
 
     return (
       <Container className="cardcontainer">
@@ -21,7 +36,7 @@ export const Cards = ({productos}) => {
                     <Card.Text>
                     $ {p.price}/kg   {p.prices}/kg
                     </Card.Text>
-                    <Button style={{ backgroundColor: "rgb(230, 154, 12)"}} variant="primary">Agregar</Button>
+                    <Button id={p.description} onClick={(e)=>onSearchid(e)} style={{ backgroundColor: "rgb(230, 154, 12)"}} variant="primary">Agregar</Button>
                 </Card.Body>
                 </Card>
           ) )}
